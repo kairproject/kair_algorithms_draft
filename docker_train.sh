4ee5c07e61ec8dec804ed98cce975cdc9fc698ec
whikwon <whikwon@gmail.com>
2019-04-09 20:32:11 +0900

    Fix rosrun execution command

--- docker_train.sh
+++ docker_train.sh
@@ -9,7 +9,7 @@ if [ "$1" == "lunarlander" ]; then
 		python run_lunarlander_continuous.py --algo $2 --off-render
 elif [ "$1" == "openmanipulator" ]; then
 	cd $KAIR/scripts; \
-		rosrun kair_algorithms run_open_manipulator_reacher_v0.py --algo $2 --off-render
+		/opt/ros/$ROS_DISTRO/bin/rosrun kair_algorithms run_open_manipulator_reacher_v0.py --algo $2 --off-render
 else
 	echo "Unknown parameter"
 fi
